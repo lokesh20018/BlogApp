83eb85af12e471e85695b8c01a715bb3018efd33
lokesh20018 <lokesh20018@gmail.com>
2021-02-28 10:01:37 +0530

    removing unnessary code

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const mongoose = require('mongoose');
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.dbUrl ;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+//const client = new MongoClient(uri, { useNewUrlParser: true });
+//client.connect(err => {
+//  const collection = client.db("test").collection("devices");
+//  // perform actions on the collection object
+//  client.close();
+//});
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
